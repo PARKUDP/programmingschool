893ad6a19a6dcaaaf9f23d33194f5e481bbee6f6
park <park@ieusernoMacBook-Air.local>
2025-02-21 19:25:59 +0900

    test code format

--- backend/tests/test_app.py
+++ backend/tests/test_app.py
@@ -5,7 +5,8 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../")))
 
 import pytest
-from app import create_app 
+from app import create_app
+
 
 @pytest.fixture
 def client():
@@ -14,10 +15,12 @@ def client():
     with app.test_client() as client:
         yield client
 
+
 def test_home(client):
     response = client.get("/")
     assert response.status_code == 200
     assert response.get_json() == {"message": "Backend is running!"}
+
 
 def test_health_check(client):
     response = client.get("/health")
