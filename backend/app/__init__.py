84684f4e9029c2d8d0762e3bd72dc1d6bb7a0bf2
park <park@ieusernoMacBook-Air.local>
2025-02-21 19:10:03 +0900

    format

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,12 +1,13 @@
 from flask import Flask, jsonify
 
+
 def create_app():
     app = Flask(__name__)
-    
+
     @app.route("/")
     def home():
         return jsonify({"message": "Backend is running!"}), 200
-    
+
     @app.route("/health", methods=["GET"])
     def health_check():
         return {"status": "OK"}, 200
